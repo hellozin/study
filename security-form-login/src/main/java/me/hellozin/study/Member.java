ac90376946ed89dfad45f1368edaf57f30de14be
hellozin <paul26375@gmail.com>
2019-08-22 22:40:13 +0900

    ADD: Member 엔티티 추가

---
+++ security-form-login/src/main/java/me/hellozin/study/Member.java
@@ -0,0 +1,32 @@
+package me.hellozin.study;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String memberId;
+
+    private String password;
+
+    private int age;
+
+    private boolean isMerried;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
